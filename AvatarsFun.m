5221cc00bab1c8810dc8c8073c4c5e21da39c300
Yifeng Tao <taoyf12@sina.com>
2016-08-23 23:49:07 -0400

    add bsf alg

--- AvatarsFun.m
+++ AvatarsFun.m
@@ -8,10 +8,10 @@ P = imresize(P,[300,300]);
 
 % show original figures.
 figure('color', [1 1 1]);
-subplot(3,2,1);
+subplot(2,4,1);
 image(G);
 title('color_1')
-subplot(3,2,2);
+subplot(2,4,5);
 image(P);
 title('color_2')
 
@@ -25,37 +25,56 @@ Ps = (P(:,:,1) > tr).*(P(:,:,2) > tr).*(P(:,:,3) > tr);
 Pb = uint8(Ps*255);
 
 colormap(gray);
-subplot(3,2,3);
+subplot(2,4,2);
 image(Gb);
 title('binary_1')
-subplot(3,2,4);
+subplot(2,4,6);
 image(Pb);
 title('binary_2')
 
-% show cropped ones.
+% show cropped ones: BFS
 [len,len] = size(Gs);
 add = 1;
 Gs = [ones(add),ones(add,len),ones(add);
     ones(len,add),Gs,ones(len,add);
     ones(add),ones(add,len),ones(add)];
-Gsd = diffuse(Gs);
+Gsd = diffuse_bfs(Gs);
 Gsd = Gsd(2:end-1,2:end-1);
 Gsd = uint8(Gsd*255);
 
-subplot(3,2,5);
+subplot(2,4,3);
 image(Gsd);
-title('cropped_1')
+title('cropped_1:BFS')
+
 
 [len,len] = size(Ps);
 add = 1;
 Ps = [ones(add),ones(add,len),ones(add);
     ones(len,add),Ps,ones(len,add);
     ones(add),ones(add,len),ones(add)];
-Psd = diffuse(Ps);
+Psd = diffuse_bfs(Ps);
 Psd = Psd(2:end-1,2:end-1);
 Psd = uint8(Psd*255);
 
-subplot(3,2,6);
+subplot(2,4,7);
 image(Psd);
-title('cropped_2')
+title('cropped_2:BFS')
+
+% show cropped ones: DFS
+Gsd = diffuse_dfs(Gs);
+Gsd = Gsd(2:end-1,2:end-1);
+Gsd = uint8(Gsd*255);
+
+subplot(2,4,4);
+image(Gsd);
+title('cropped_1:DFS')
+
+
+Psd = diffuse_dfs(Ps);
+Psd = Psd(2:end-1,2:end-1);
+Psd = uint8(Psd*255);
+
+subplot(2,4,8);
+image(Psd);
+title('cropped_2:DFS')
 % Q.E.D.
